bc7b7f595426004cab1dfe6f7ac63ff8e1b3eb84
Arman Singh <109358065+ArmanSingh24@users.noreply.github.com>
2023-07-16 09:05:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,8 @@ const clientId = CLIENT_ID;
 const clientSecret = CLIENT_SECRET;
 const refreshToken = REFRESH_TOKEN;
 let accessToken = ''; // Stores the current Access Token
-let isFetchingTrack = false; // Flag to track if track information is already being fetched
-let isRefreshingToken = false; // Flag to track if the access token is already being refreshed
-
 // Function to refresh the Access Token using the Refresh Token
 async function refreshAccessToken() {
-  if (isRefreshingToken) {
-    return; // If already refreshing token, skip this call
-  }
-
-  isRefreshingToken = true; // Set the flag to true to indicate that token refresh is in progress
-
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     headers: {
@@ -28,29 +19,18 @@ async function refreshAccessToken() {
     });
 
     accessToken = response.data.access_token;
-    isRefreshingToken = false; // Reset the flag after successful token refresh
   } catch (error) {
     console.error('Error refreshing Access Token:', error);
-    isRefreshingToken = false; // Reset the flag if token refresh fails
   }
 }
-
 // Function to fetch and display the currently playing track
 function updateTrack() {
-  if (isFetchingTrack) {
-    return; // If already fetching track information, skip this call
-  }
-
-  isFetchingTrack = true; // Set the flag to true to indicate that track info is being fetched
-
   fetch('https://api.spotify.com/v1/me/player/currently-playing', {
     headers: {
       'Authorization': `Bearer ${accessToken}`
     }
   })
     .then(response => {
-      isFetchingTrack = false; // Reset the flag when the request is complete
-
       if (response.status === 204) {
         throw new Error('No currently playing track');
       }
@@ -68,8 +48,6 @@ function updateTrack() {
       `;
     })
     .catch(error => {
-      isFetchingTrack = false; // Reset the flag in case of an error
-
       if (error.message !== 'No currently playing track') {
         console.log(error);
       }
@@ -78,11 +56,8 @@ function updateTrack() {
 }
 
 // Update the track information every 1 seconds
-setInterval(updateTrack, 1000); // Change to 5000 milliseconds (5 seconds)
-
+setInterval(updateTrack, 1000);
 // Initial token refresh
 refreshAccessToken();
-
 // Refresh the token every hour (3600 seconds)
 setInterval(refreshAccessToken, 3600000);
-    
